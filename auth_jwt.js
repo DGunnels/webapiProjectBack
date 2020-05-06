9ee08ab5cb9258702f4157bd47f683f05a78c182
Derek Gunnels <45681663+DGunnels@users.noreply.github.com>
2020-05-05 21:41:38 -0600

    Fixed reqs

--- auth_jwt.js
+++ auth_jwt.js
@@ -3,7 +3,6 @@ var passport = require('passport');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var User = require('./Users');
-var Movie = require('./movies');
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
 opts.secretOrKey = process.env.SECRET_KEY;
